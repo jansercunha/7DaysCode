2071984006c0507c266046510165bf66fd171ad1
Janser <jansercunha@gmail.com>
2022-12-15 18:34:15 -0400

    Dia 4 - Gerando o HTML

---
+++ src/main/java/br/com/alura/dayscode/HTMLGenerator.java
@@ -0,0 +1,67 @@
+package br.com.alura.dayscode;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import br.com.alura.dayscode.model.Movie;
+
+public class HTMLGenerator {
+
+	private PrintWriter printWriter;
+
+	public HTMLGenerator(PrintWriter printWriter) {
+		this.printWriter = printWriter;
+	}
+
+	String head =
+			"""
+			<head>
+				<meta charset=\"utf-8\">
+				<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+				<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css\" 
+					+ "integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">					
+			</head>
+			""";
+	
+	String divTemplate =
+			"""
+				<div class=\"card text-white bg-dark mb-3\" style=\"max-width: 18rem;\">
+					<h4 class=\"card-header\">%s</h4>
+					<div class=\"card-body\">
+						<img class=\"card-img\" src=\"%s\" alt=\"%s\">
+						<p class=\"card-text mt-2\">Nota: %s - Ano: %s</p>
+					</div>
+				</div>
+			""";
+
+	public void generate(List<Movie> items) {
+		/*
+		 * Gravação Tag inicial
+		 */
+		printWriter.println("<html>");
+		
+		/*
+		 * Gravando Head
+		 */
+		printWriter.println(head);
+		
+		/*
+		 * Gravando corpo
+		 */
+		printWriter.println("<body>");
+		items.forEach(movie -> {
+			//usando o template com os dados do filme			
+			printWriter.println(String.format(divTemplate, movie.getTitle(), movie.getImage(), movie.getTitle(), movie.getImDbRating(), movie.getYear()));
+		});
+		
+		/*
+		 * Fechamento
+		 */
+		printWriter.println("""
+				</body>
+				</html>
+				""");
+	}
+
+			
+}
--- src/main/java/br/com/alura/dayscode/controller/ImdbController.java
+++ src/main/java/br/com/alura/dayscode/controller/ImdbController.java
@@ -1,5 +1,7 @@
 package br.com.alura.dayscode.controller;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import br.com.alura.dayscode.HTMLGenerator;
 import br.com.alura.dayscode.model.Movie;
 
 @RestController
@@ -32,13 +35,18 @@ public class ImdbController {
 	private record ListOfMovies(List<Movie> items) {}
 
 	@GetMapping("/top250")
-	public List<Movie> getTop250Filmes() {
+	public List<Movie> getTop250Filmes() throws FileNotFoundException {
 
 		try {
 			URI urlCompleta = new URI(url.concat("/").concat(top250).concat("/").concat(token));
 			ResponseEntity<ListOfMovies> responseEntity = restTemplate.getForEntity(urlCompleta, ListOfMovies.class);
 			
-			System.out.println(responseEntity.getBody());
+//			System.out.println(responseEntity.getBody());
+			
+			PrintWriter printWriter = new PrintWriter("src/main/resources/top250.html");
+			new HTMLGenerator(printWriter).generate(responseEntity.getBody().items());
+			printWriter.close();
+			
 			return responseEntity.getBody().items();
 			
 		} catch (URISyntaxException e) {
